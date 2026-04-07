fa573ae55ec12b03c5e64e683c55f5ca0f80ad4f
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-07 21:06:12 +0300

    create and implement: union func

---
+++ utils.go
@@ -0,0 +1,25 @@
+package gobayes
+
+// union combine deux listes de chaînes de caractères en gardant l'ordre d'apparition
+func union(a, b []string) []string {
+	m := make(map[string]bool)
+	var result []string
+
+	// On ajoute les éléments de la première liste
+	for _, item := range a {
+		if !m[item] {
+			m[item] = true
+			result = append(result, item)
+		}
+	}
+
+	// On ajoute les éléments de la deuxième liste s'ils n'existent pas encore
+	for _, item := range b {
+		if !m[item] {
+			m[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
