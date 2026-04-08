c14e2b07887d99159ac7ac49b0811bd0d532edba
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-08 11:10:46 +0300

    remove: old code

--- inference.go
+++ inference.go
@@ -5,35 +5,6 @@ import "fmt"
 // Query calcule la distribution de probabilité d'une variable cible,
 // compte tenu des preuves fournies (evidence).
 func (n *Network) Query(target string, evidence map[string]int) *Factor {
-	// 1. Initialiser les facteurs à partir des CPDs de chaque nœud
-	// factors := []*Factor{}
-	// for _, node := range n.Nodes {
-	// 	factors = append(factors, node.ToFactor())
-	// }
-
-	// // 2. Réduire les facteurs avec les preuves
-	// for variable, state := range evidence {
-	// 	for i, f := range factors {
-	// 		factors[i] = f.Reduce(variable, state)
-	// 		fmt.Println("partie reduce", factors[i])
-	// 	}
-	// }
-
-	// // 3. Multiplier tous les facteurs entre eux
-	// // result := factors[0]
-	// // for i := 1; i < len(factors); i++ {
-	// // 	result = result.Multiply(factors[i])
-	// // }
-	// // 3. Multiplier tous les facteurs entre eux de manière sécurisée
-    // var result *Factor
-    // for _, f := range factors {
-    //     if result == nil {
-    //         result = f
-    //     } else {
-    //         result = result.Multiply(f)
-    //     }
-    // }
-
 	factors := []*Factor{}
     for _, node := range n.Nodes {
         f := node.ToFactor()
